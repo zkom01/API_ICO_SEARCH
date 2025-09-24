b7bc8ad18124eceec5f505e3cb42cdac2657e7b2
zkom01 <komarek.zdenek@seznam.cz>
2025-09-24 21:57:13 +0200

    upraven styl eda.json

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ WIDTH = 440
 HEIGHT = 470
 
 FONT_NADPIS = "Helvetica", 18, "bold"
-FONT_TEXT = "Verdana", 14
+FONT_TEXT = "Helvetica", 14, "bold"
 FONT_BUTTONS = "Helvetica", 14, "bold"
 FONT_NADPIS1 = "Helvetica", 20, "bold"
